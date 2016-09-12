5c3def2140684c0a07a2f385315626c8291f8e98
morrisonja <morrisonja@users.noreply.github.com>
2016-09-13 02:10:30 +1000

    Make React loop available to bots

--- src/PhpSlackBot/Command/BaseCommand.php
+++ src/PhpSlackBot/Command/BaseCommand.php
@@ -3,8 +3,13 @@ namespace PhpSlackBot\Command;
 
 abstract class BaseCommand extends \PhpSlackBot\Base {
 
+    protected $loop = null;
+
     public function executeCommand($message, $context) {
         return $this->execute($message, $context);
     }
-
+    
+    public function setLoop($loop) {
+        $this->loop = $loop;
+    }
 }
